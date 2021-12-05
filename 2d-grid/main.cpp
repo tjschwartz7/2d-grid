30df37324f51bb11dd8dcfa1c02c902c8a1c1205
tjschwartz7 <54190751+tjschwartz7@users.noreply.github.com>
2021-12-04 20:57:48 -0500

    Update main.cpp
    just change the variable(s) for the ascii graph to change each instance in which it is used (groundbreaking content)

--- 2d-grid/main.cpp
+++ 2d-grid/main.cpp
@@ -1,4 +1,6 @@
 
+
+
 #include <stdio.h>
 
 int main()
@@ -13,15 +15,17 @@ int main()
 	printf("(2)y=Ax^2+Bx+C\n");
 	printf("(3)y=Ax^3+Bx^2+Cx+D\n");
 	scanf_s("%d", &opt);
-	int x,y, A, B,C,D = 0;
+	int x, y, A, B, C, D = 0;
 	printf("A,B,C,D=");
 	scanf_s("%d%d%d%d", &A, &B, &C, &D);
+	char background[3] = "+ ";
+	char line[3] = "# ";
 
 
-	
+
 	for (y = ySize - (ySize / 2); y >= -1 * (ySize - (ySize / 2)); y--)
 	{
-		
+
 		for (x = -1 * (xSize - (xSize - 1) / 2); x <= (xSize - (xSize / 2)); x++)
 		{
 			switch (opt)
@@ -29,27 +33,27 @@ int main()
 
 			case 0:
 				if (y == A)
-					printf("# ");
-				else printf("+ ");
+					printf(line);
+				else printf(background);
 				break;
 			case 1:
 				if (y == A * x + B)
-					printf("# ");
-				else printf("+ ");
+					printf(line);
+				else printf(background);
 				break;
 			case 2:
 				if (y == A * x * x + B * x)
-					printf("# ");
-				else printf("+ ");
+					printf(line);
+				else printf(background);
 				break;
 			case 3:
-				if (y == A * x *x*x+ B*x*x+C*x+D)
-					printf("# ");
-				else printf("+ ");
+				if (y == A * x * x * x + B * x * x + C * x + D)
+					printf(line);
+				else printf(background);
 				break;
 
 			}
-			
+
 		}
 		printf("\n");
 	}
